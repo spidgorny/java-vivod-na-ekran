c7f0b8dcc4dfcfc0461b5bbde1e4644c54e6294e
Slawa Pidgorny <spidgorny@gmail.com>
2019-04-10 10:28:08 +0200

    - add safe asking for yes/no questions

--- src/NaEkran1.java
+++ src/NaEkran1.java
@@ -7,8 +7,6 @@ public class NaEkran1 {
         String Tv2 = "Samsung Smart-TV";
         String Tv3 = "Panasonic Smart-TV";
 
-        String JA = "Ja";
-
         double preis1 = 399.99;
         double preis2 = 419.99;
         double preis3 = 549.99;
@@ -19,6 +17,8 @@ public class NaEkran1 {
         int id2 = 2;
         int id3 = 3;
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Herzich willkommen in TV-Abteilung! \n");
 
         System.out.println("Heute haben wir einen " + Tv1 + " fuer den Preis von " + preis1 + " €\n" + Tv2
@@ -28,17 +28,14 @@ public class NaEkran1 {
         System.out.println("Was gefällt Sie am besten? " + "Bitte eingeben:"
                 + "\n 1 = Philips, \n 2 = Samsung oder \n 3 = Panasonic \n");
 
-        Scanner kunde = new Scanner(System.in);
-        int bestellung = kunde.nextInt();
+        int bestellung = scanner.nextInt();
 //        println("Auswahl: " + );
         if (bestellung == 1) {
             println("Eine gute Wahl! \n");
             println("Haben Sie eine Kundenkarte? " + "Bitte eingeben:" + "\n Y = Ja, oder N = Nein \n");
         }
 
-        Scanner karte = new Scanner(System.in);
-        String kundenkarte = karte.nextLine();
-        if (kundenkarte.equals(JA)) {
+        if (askBool()) {
             println("Super! Sie bekommen zusätzlich noch 15%-Rabbatt, "
                     + "\n den aktuellen Preis beträgt dann " + (preis1 - preis1 * 0.15) + " € \n");
             println("Suchen Sie nach Ihrer individuellen Finanzierung? \n"
@@ -47,9 +44,7 @@ public class NaEkran1 {
             println("Sie können den Betrag " + (preis1 - preis1 * 0.15) + " € direkt an der Kasse bezahlen?");
         }
 
-        Scanner kredit = new Scanner(System.in);
-        String kundenkredit = kredit.nextLine();
-        if (kundenkredit.equals(JA)) {
+        if (askBool()) {
             println("Bitte, wenden Sie sich direkt an die Kreditabteilung!");
         } else {
             println("Sie können den Betrag " + preis1 + " € direkt an der Kasse bezahlen?");
@@ -67,6 +62,49 @@ public class NaEkran1 {
 
     static void println(String text) {
         System.out.println(text);
+    }
+
+    static boolean askBool() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            String kundenkarte = scanner.nextLine();
+            Boolean yes = checkAnswer(kundenkarte);
+            if (yes != null) {
+                if (yes) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            println("Nicht verstanden. Ja oder nein?");
+        } while (true);
+    }
+
+    static Boolean checkAnswer(String answer) {
+        String JA = "ja";
+        String NEIN = "nein";
+
+        if (answer.length() == 0) {
+            return null;
+        }
+
+        if (answer.toLowerCase().equals(JA)) {
+            return true;
+        }
+        if ((answer.toCharArray()[0] + "").toLowerCase().equals("j")) {
+            return true;
+        }
+        if ((answer.toCharArray()[0] + "").toLowerCase().equals("y")) {
+            return true;
+        }
+
+        if (answer.toLowerCase().equals(NEIN)) {
+            return false;
+        }
+        if ((answer.toCharArray()[0] + "").toLowerCase().equals("n")) {
+            return false;
+        }
+        return null;
     }
 
 }
